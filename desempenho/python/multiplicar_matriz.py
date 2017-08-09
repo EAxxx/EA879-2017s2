6e7479d697e12d7d6213522ac5f97cad47e0ae67
Tiago <tiagoft@gmail.com>
2017-08-09 09:09:08 -0300

    [master] Adicionei codigo para comparacao de desempenho em multiplicacao de matrizes

---
+++ desempenho/python/multiplicar_matriz.py
@@ -0,0 +1,20 @@
+
+import time
+
+t = time.time()
+
+import sys
+import numpy as np
+
+t0 = time.time()
+A = np.random.random( (int(sys.argv[1]), int(sys.argv[1])) )
+
+t1 = time.time()
+B = np.dot(A,A)
+
+t2 = time.time()
+
+print "Teste com matriz quadrada de NxN, N=", sys.argv[1]
+print "Carregar bibliotecas: ", 1000*(t0-t)
+print "Criar matriz: ", 1000*(t1-t0)
+print "Processar: ", 1000*(t2-t1)
